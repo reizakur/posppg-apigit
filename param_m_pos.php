77d843512ed75e51cefbde7fb34a47dc4aca43d6
reizakur <rheizakurniasmk1@gmail.com>
2023-10-02 16:43:24 +0700

    asda

--- param_m_pos.php
+++ param_m_pos.php
@@ -21,26 +21,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         echo("mati hatiiii");
     // Loop through the array
     foreach ($data as $item) {
-        echo "ID: " . $item['id'] . "<br>";
-        echo "POS ID: " . $item['pos_id'] . "<br>";
-        echo "Product ID: " . $item['product_id'] . "<br>";
-        echo "Quantity: " . $item['qty'] . "<br>";
-        echo "Price: " . $item['price'] . "<br>";
-        echo "Tax Amount: " . $item['taxamt'] . "<br>";
-        echo "Discount Amount: " . $item['discountamt'] . "<br>";
-        echo "Price List: " . $item['pricelist'] . "<br>";
-        echo "Transaction Time: " . $item['trxtime'] . "<br>";
-        echo "Created At: " . $item['created_at'] . "<br>";
-        echo "Updated At: " . $item['updated_at'] . "<br>";
-        echo "Created By: " . $item['created_by'] . "<br>";
-        echo "Updated By: " . $item['updated_by'] . "<br>";
-        echo "<hr>";
+        echo("damn indonesia");
+        // echo "ID: " . $item['id'] . "<br>";
+        // echo "POS ID: " . $item['pos_id'] . "<br>";
+        // echo "Product ID: " . $item['product_id'] . "<br>";
+        // echo "Quantity: " . $item['qty'] . "<br>";
+        // echo "Price: " . $item['price'] . "<br>";
+        // echo "Tax Amount: " . $item['taxamt'] . "<br>";
+        // echo "Discount Amount: " . $item['discountamt'] . "<br>";
+        // echo "Price List: " . $item['pricelist'] . "<br>";
+        // echo "Transaction Time: " . $item['trxtime'] . "<br>";
+        // echo "Created At: " . $item['created_at'] . "<br>";
+        // echo "Updated At: " . $item['updated_at'] . "<br>";
+        // echo "Created By: " . $item['created_by'] . "<br>";
+        // echo "Updated By: " . $item['updated_by'] . "<br>";
+        // echo "<hr>";
     }
 }
     // Insert data into the database (replace with your actual table and column names)
     $sql = "INSERT INTO your_table_name (company, geturl, id_m_warehouse_icpl) VALUES (?, ?, ?)";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sss", $company, $geturl, $id_m_warehouse_icpl);
+    $stmt->bind_param("", $company, $geturl, $id_m_warehouse_icpl);
 
     if ($stmt->execute()) {
         echo "Data inserted successfully.";
