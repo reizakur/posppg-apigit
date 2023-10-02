ceebff5f0898ec0d74f33f673b4d410a6b44eb9c
reizakur <rheizakurniasmk1@gmail.com>
2023-10-02 15:40:33 +0700

    asda

--- param_m_pos.php
+++ param_m_pos.php
@@ -1,16 +1,18 @@
 <?php
+include "koneksi.php";
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $company = $_POST['company']; 
     $geturl = $_POST['geturl']; 
-    $id_m_warehouse_icpl = $_POST['id_m_warehouse_icpl'];
-    $dbConnection = new mysqli("hostname", "username", "password", "database_name");
-      if ($dbConnection->connect_error) {
-        die("Connection failed: " . $dbConnection->connect_error);
-    }
+    $datampos = $_POST['datampos'];
+    $datamposline = $_POST['datamposline'];
+    echo($datampos);
+    echo($datamposline);
+    die();
+    
 
     // Insert data into the database (replace with your actual table and column names)
     $sql = "INSERT INTO your_table_name (company, geturl, id_m_warehouse_icpl) VALUES (?, ?, ?)";
-    $stmt = $dbConnection->prepare($sql);
+    $stmt = $conn->prepare($sql);
     $stmt->bind_param("sss", $company, $geturl, $id_m_warehouse_icpl);
 
     if ($stmt->execute()) {
