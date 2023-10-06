c2d7b8ab900bc112e00dc53abd15d9cbc0dec43c
reizakur <rheizakurniasmk1@gmail.com>
2023-10-06 14:39:10 +0700

    gfdfds

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
 $servername = "localhost"; // Replace with your database server hostname or IP address
-$username = "dara1799@_dummy"; // Replace with your database username
-$password = "4215kurnia"; // Replace with your database password
-$database = "dara1799@_dummy"; // Replace with your database name
+$username = "dara1799_dummy"; // Replace with your database username
+$password = "4215Kurnia"; // Replace with your database password
+$database = "dara1799_dummy"; // Replace with your database name
 
 // Create a database connection
 $conn = new mysqli($servername, $username, $password, $database);
