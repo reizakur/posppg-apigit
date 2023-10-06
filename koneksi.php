3ba39561a29f9e9d5370b4c5e69a9927ca109419
reizakur <rheizakurniasmk1@gmail.com>
2023-10-06 14:54:40 +0700

    ssd

--- koneksi.php
+++ koneksi.php
@@ -1,27 +1,8 @@
 <?php
-// Database configuration
-$servername = "localhost"; // Replace with your database server hostname or IP address
-$username = "dara1799_dummy"; // Replace with your database username
-$password = "jakarta@1999"; // Replace with your database password
-$database = "dara1799_dummy"; // Replace with your database name
-//
-// Create a database connection
-$conn = new mysqli($servername, $username, $password, $database);
+$con = mysqli_connect("localhost","dara1799_dummy","jakara@1999","dara1799_dummy");
 
-// Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (mysqli_connect_error ())
+{
+	echo "Failed broooo" .mysqli_connect_error();
 }
-
-// Optionally, you can set the character set (UTF-8 in this example)
-$conn->set_charset("utf8");
-
-// You can now use the "$conn" variable for database queries
-
-// Example query
-$query = "SELECT * FROM your_table";
-$result = $conn->query($query);
-
-// Close the database connection when you're done
-$conn->close();
 ?>
