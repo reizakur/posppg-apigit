ea1a6e81f1b2a6fa65bcf618af24915494c62944
reizakur <rheizakurniasmk1@gmail.com>
2023-10-09 14:36:12 +0700

    wakawaw

--- param_end_of_day.php
+++ param_end_of_day.php
@@ -5,15 +5,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $company = $_POST['company'];
     $geturl = $_POST['geturl'];
     $dataendofday =  $_POST['dataendofday'];
-    echo ($dataendofday);
-
-    echo ('proses aja');
+ 
     // Convert JSON data to an associative array
     $data = json_decode($_POST['dataendofday'],true);
-    echo($data);
+    
     // Check if JSON decoding was successful
-    if ($data != null) {
-echo('bener kok');
+    if ($data != null) { 
         // Loop through the data
         foreach ($data['data'] as $item) {
             $id = $item['id'];
@@ -43,15 +40,7 @@ echo('bener kok');
               
                 echo "m_pos gagal";
                  echo "Error: " . mysqli_error($con);  
-            }
-            // Perform database insertions here for each record
-            // Example code for inserting into m_end_of_daylog table
-           
-            if ($result) {
-                echo "Record inserted successfully into m_end_of_daylog table.<br>";
-            } else {
-                echo "Error: " . mysqli_error($con);
-            }
+            }        
         }
     } else {
         // JSON decoding failed
