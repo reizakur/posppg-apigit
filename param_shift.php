9833131760e6913a6b471e196832a4cda0ca1209
reizakur <rheizakurniasmk1@gmail.com>
2023-10-09 15:47:52 +0700

    asdad

--- param_shift.php
+++ param_shift.php
@@ -51,7 +51,7 @@ foreach ($data as $item) {
          echo "Error: " . mysqli_error($con);  
     }
     echo('ayam 234'); 
-    echo($item['datamshiftbonmanual']);
+    echo($item['datamshiftbonmanual']['modal1']);
     foreach ($item['datamshiftbonmanual'] as $shiftBonManual) {
         echo('kucing 111'); 
         $id = $shiftBonManual['id'];
