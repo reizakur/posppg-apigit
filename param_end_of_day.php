07072229c11ed5f306adf2865f0d5493239f205b
reizakur <rheizakurniasmk1@gmail.com>
2023-10-09 13:43:48 +0700

    adsadasd

--- param_end_of_day.php
+++ param_end_of_day.php
@@ -1,61 +1,50 @@
 <?php
-//satu m_shiflog bisa banyak m_shift_bon_ambual
-include ("koneksi.php");
+// Include your database connection code (koneksi.php) here
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $company = $_POST['company']; 
-    $geturl = $_POST['geturl']; 
+    // Retrieve POST data
+    $company = $_POST['company'];
+    $geturl = $_POST['geturl'];
     $dataendofday = $_POST['dataendofday'];
-    // echo($company);
-    echo($dataendofday);
-        echo("hmmmmm");
-  $jsonData = $dataendofday;  
-  $data = json_decode($jsonData, true);  
-  if ($data != null) {
-    echo("kemana sii");
-    // Access the data as needed
-    // foreach ($data['data'] as $item) {
-    //     $id = $item['id'];
-    //     $warehouseId = $item['warehouse_id'];
-    //     $cashierId = $item['cashier_id']; 
-    // }  
-} else {
-    // JSON decoding failed
-    echo "JSON decoding error";
+
+    // Convert JSON data to an associative array
+    $data = json_decode($dataendofday, true);
+
+    // Check if JSON decoding was successful
+    if ($data !== null) {
+        // Loop through the data
+        foreach ($data['data'] as $item) {
+            $id = $item['id'];
+            $warehouseId = $item['warehouse_id'];
+            $cashierId = $item['cashier_id'];
+            $datetrx = $item['datetrx'];
+            $cash = $item['cash'];
+            $none_cash = $item['none_cash'];
+            $cash_cashier = $item['cash_cashier'];
+            $rek_jw = $item['rek_jw'];
+            $rek_tax = $item['rek_tax'];
+            $syncedbi = $item['syncedbi'];
+            $created_at = $item['created_at'];
+            $updated_at = $item['updated_at'];
+            $created_by = $item['created_by'];
+            $updated_by = $item['updated_by'];
+
+            // Perform database insertions here for each record
+            // Example code for inserting into m_end_of_daylog table
+            $sql = "INSERT INTO m_end_of_daylog (warehouse_id, cashier_id, datetrx, cash, none_cash, cash_cashier, rek_jw, rek_tax, syncedbi, created_at, updated_at, created_by, updated_by) 
+                    VALUES ('$warehouseId', '$cashierId', '$datetrx', '$cash', '$none_cash', '$cash_cashier', '$rek_jw', '$rek_tax', '$syncedbi', '$created_at', '$updated_at', '$created_by', '$updated_by')";
+            
+            $result = mysqli_query($con, $sql);
+
+            if ($result) {
+                echo "Record inserted successfully into m_end_of_daylog table.<br>";
+            } else {
+                echo "Error: " . mysqli_error($con);
+            }
+        }
+    } else {
+        // JSON decoding failed
+        echo "JSON decoding error";
+    }
 }
-
-  echo("hmmmmm321");
-  echo($data);
-
-foreach($data as $jsonData){
-
-$id = $jsonData['id'];
-$warehouse_id = $jsonData['warehouse_id'];
-$cashier_id = $jsonData['cashier_id'];
-$datetrx = $jsonData['datetrx'];
-$cash = $jsonData['cash'];
-$none_cash = $jsonData['none_cash'];
-$cash_cashier = $jsonData['cash_cashier'];
-$rek_jw = $jsonData['rek_jw'];
-$rek_tax = $jsonData['rek_tax'];
-$syncedbi = $jsonData['syncedbi'];
-$created_at = $jsonData['created_at'];
-$updated_at = $jsonData['updated_at'];
-$created_by = $jsonData['created_by'];
-$updated_by = $jsonData['updated_by']; 
-
-$sqlsatu = mysqli_query($con,"insert into m_end_of_daylog 
-(warehouse_id, cashier_id, datetrx, cash, none_cash, cash_cashier, rek_jw, rek_tax, syncedbi, created_at, updated_at, created_by, updated_by)
-values ('$warehouse_id', '$cashier_id', '$datetrx', '$cash', '$none_cash', '$cash_cashier', '$rek_jw', '$rek_tax', '$syncedbi', '$created_at', '$updated_at', '$created_by', '$updated_by')");
-$go=mysqli_query($con,$sqlsatu); 
-
-if($sqlsatu){
-    echo "m_end_of_daylog berhasil";
-    echo "Record inserted successfully yang m_psnya.<br>";
- 
-}else{              
-    echo "m_end_of_daylog gagal";  
-}
-
-}}
- 
 ?>
