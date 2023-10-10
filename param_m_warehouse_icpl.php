e61f68bb3ceb6cce70f540009a4e3c781cad90a7
reizakur <rheizakurniasmk1@gmail.com>
2023-10-10 17:34:12 +0700

    adasdsadadsad

--- param_m_warehouse_icpl.php
+++ param_m_warehouse_icpl.php
@@ -7,55 +7,449 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $senderin = $_POST['sender'];
     echo $id_m_warehouse_icpl;
     echo $senderin;
-    // if($id_m_warehouse_icpl == 'BP063302'){
-    //  $data = [
-    //  [
-    //     "id" => 1,
-    //     "warehouse_id"=> BP063302,
-    //     "icpl"=> "ICPL01",
-    //     "validfrom"=> "2023-01-03 00:00:00",
-    //     "active"=> "Y"
-    // ],
-    // [
-    //     "id"=> 3,
-    //     "warehouse_id"=> BP063302,
-    //     "icpl"=> "ICPL03",
-    //     "validfrom"=> "2023-09-01 00:00:00",
-    //     "active"=> "Y"
-    // ]
-    // ];
+    if($id_m_warehouse_icpl == '1500434P'){
+     $data = [
+     [
+        "id" => 1,
+        "warehouse_id"=> "1500434P",
+        "icpl"=> "ICPL01",
+        "validfrom"=> "2023-01-03 00:00:00",
+        "active"=> "Y"
+    ],
+    [
+        "id"=> 3,
+        "warehouse_id"=> "1500434P",
+        "icpl"=> "ICPL03",
+        "validfrom"=> "2023-09-01 00:00:00",
+        "active"=> "Y"
+    ]
+    ];
 
-    // }else if ($id_m_warehouse_icpl == 'R00TEST'){
-    //   $data = [
-    //         [
-    //           "id" => 1,
-    //           "warehouse_id"=> 'R00TEST',
-    //           "icpl"=> "ICPL015",
-    //           "validfrom"=> "2023-01-03 00:00:00",
-    //           "active"=> "Y"
-    //       ],
-    //       [
-    //           "id"=> 3,
-    //           "warehouse_id"=> 3,
-    //           "icpl"=> "ICPL013",
-    //           "validfrom"=> "2023-09-01 00:00:00",
-    //           "active"=> "Y"
-    //       ]
-    //       ];
-    // }else{
-    //   return $response = [
-    //         "status" => 200,
-    //         "message" => "Bisa Kok Cuman ID nya ga kecatet"
-    //     ];
-    // }
+    }else if($id_m_warehouse_icpl == '1500525P'){
+     $data = [
+     [
+        "id" => 1,
+        "warehouse_id"=> "1500525P",
+        "icpl"=> "ICPL01",
+        "validfrom"=> "2023-01-03 00:00:00",
+        "active"=> "Y"
+    ],
+    [
+        "id"=> 3,
+        "warehouse_id"=>"1500525P",
+        "icpl"=> "ICPL03",
+        "validfrom"=> "2023-09-01 00:00:00",
+        "active"=> "Y"
+    ]
+    ];
+
+    } else if($id_m_warehouse_icpl == '1500526P'){
+        $data = [
+        [
+           "id" => 1,
+           "warehouse_id"=> "1500526P",
+           "icpl"=> "ICPL01",
+           "validfrom"=> "2023-01-03 00:00:00",
+           "active"=> "Y"
+       ],
+       [
+           "id"=> 3,
+           "warehouse_id"=>"1500526P",
+           "icpl"=> "ICPL03",
+           "validfrom"=> "2023-09-01 00:00:00",
+           "active"=> "Y"
+       ]
+       ];
+   
+    }else if($id_m_warehouse_icpl == '1500584P'){
+        $data = [
+        [
+           "id" => 1,
+           "warehouse_id"=> "1500584P",
+           "icpl"=> "ICPL01",
+           "validfrom"=> "2023-01-03 00:00:00",
+           "active"=> "Y"
+       ],
+       [
+           "id"=> 3,
+           "warehouse_id"=>"1500584P",
+           "icpl"=> "ICPL03",
+           "validfrom"=> "2023-09-01 00:00:00",
+           "active"=> "Y"
+       ]
+       ];
+   
+    }else if($id_m_warehouse_icpl == '1500414P'){
+        $data = [
+        [
+           "id" => 1,
+           "warehouse_id"=> "1500414P",
+           "icpl"=> "ICPL01",
+           "validfrom"=> "2023-01-03 00:00:00",
+           "active"=> "Y"
+       ],
+       [
+           "id"=> 3,
+           "warehouse_id"=>"1500414P",
+           "icpl"=> "ICPL03",
+           "validfrom"=> "2023-09-01 00:00:00",
+           "active"=> "Y"
+       ]
+       ];
+   
+    }else if($id_m_warehouse_icpl == '1500608P'){
+        $data = [
+        [
+           "id" => 1,
+           "warehouse_id"=> "1500608P",
+           "icpl"=> "ICPL01",
+           "validfrom"=> "2023-01-03 00:00:00",
+           "active"=> "Y"
+       ],
+       [
+           "id"=> 3,
+           "warehouse_id"=>"1500608P",
+           "icpl"=> "ICPL03",
+           "validfrom"=> "2023-09-01 00:00:00",
+           "active"=> "Y"
+       ]
+       ];
+   
+    }else if($id_m_warehouse_icpl == '1500495P'){
+        $data = [
+        [
+           "id" => 1,
+           "warehouse_id"=> "1500495P",
+           "icpl"=> "ICPL01",
+           "validfrom"=> "2023-01-03 00:00:00",
+           "active"=> "Y"
+       ],
+       [
+           "id"=> 3,
+           "warehouse_id"=>"1500495P",
+           "icpl"=> "ICPL03",
+           "validfrom"=> "2023-09-01 00:00:00",
+           "active"=> "Y"
+       ]
+       ];
+   
+    }else if($id_m_warehouse_icpl == '1558023P'){
+     $data = [
+     [
+        "id" => 1,
+        "warehouse_id"=> "1558023P",
+        "icpl"=> "ICPL01",
+        "validfrom"=> "2023-01-03 00:00:00",
+        "active"=> "Y"
+    ],
+    [
+        "id"=> 3,
+        "warehouse_id"=>"1558023P",
+        "icpl"=> "ICPL03",
+        "validfrom"=> "2023-09-01 00:00:00",
+        "active"=> "Y"
+    ]
+    ];
+
+}else if($id_m_warehouse_icpl == '2500434P'){
+        $data = [
+        [
+           "id" => 1,
+           "warehouse_id"=> "2500434P",
+           "icpl"=> "ICPL01",
+           "validfrom"=> "2023-01-03 00:00:00",
+           "active"=> "Y"
+       ],
+       [
+           "id"=> 3,
+           "warehouse_id"=>"2500434P",
+           "icpl"=> "ICPL03",
+           "validfrom"=> "2023-09-01 00:00:00",
+           "active"=> "Y"
+       ]
+       ];
+   
+    }else if($id_m_warehouse_icpl == '2500525P'){
+        $data = [
+        [
+           "id" => 1,
+           "warehouse_id"=> "2500525P",
+           "icpl"=> "ICPL01",
+           "validfrom"=> "2023-01-03 00:00:00",
+           "active"=> "Y"
+       ],
+       [
+           "id"=> 3,
+           "warehouse_id"=>"2500525P",
+           "icpl"=> "ICPL03",
+           "validfrom"=> "2023-09-01 00:00:00",
+           "active"=> "Y"
+       ]
+       ];
+   
+    }else if($id_m_warehouse_icpl == '2500526P'){
+        $data = [
+        [
+           "id" => 1,
+           "warehouse_id"=> "2500526P",
+           "icpl"=> "ICPL01",
+           "validfrom"=> "2023-01-03 00:00:00",
+           "active"=> "Y"
+       ],
+       [
+           "id"=> 3,
+           "warehouse_id"=>"2500526P",
+           "icpl"=> "ICPL03",
+           "validfrom"=> "2023-09-01 00:00:00",
+           "active"=> "Y"
+       ]
+       ];
+   
+    }else if($id_m_warehouse_icpl == '500434P'){
+        $data = [
+        [
+           "id" => 1,
+           "warehouse_id"=> "500434P",
+           "icpl"=> "ICPL01",
+           "validfrom"=> "2023-01-03 00:00:00",
+           "active"=> "Y"
+       ],
+       [
+           "id"=> 3,
+           "warehouse_id"=>"500434P",
+           "icpl"=> "ICPL03",
+           "validfrom"=> "2023-09-01 00:00:00",
+           "active"=> "Y"
+       ]
+       ];
+   
+    }else if($id_m_warehouse_icpl == '2500414P'){
+        $data = [
+        [
+           "id" => 1,
+           "warehouse_id"=> "2500414P",
+           "icpl"=> "ICPL01",
+           "validfrom"=> "2023-01-03 00:00:00",
+           "active"=> "Y"
+       ],
+       [
+           "id"=> 3,
+           "warehouse_id"=>"2500414P",
+           "icpl"=> "ICPL03",
+           "validfrom"=> "2023-09-01 00:00:00",
+           "active"=> "Y"
+       ]
+       ];
+   
+    }else if($id_m_warehouse_icpl == '2500608P'){
+        $data = [
+        [
+           "id" => 1,
+           "warehouse_id"=> "2500608P",
+           "icpl"=> "ICPL01",
+           "validfrom"=> "2023-01-03 00:00:00",
+           "active"=> "Y"
+       ],
+       [
+           "id"=> 3,
+           "warehouse_id"=>"2500608P",
+           "icpl"=> "ICPL03",
+           "validfrom"=> "2023-09-01 00:00:00",
+           "active"=> "Y"
+       ]
+       ];
+   
+       }else if($id_m_warehouse_icpl == '2500495P'){
+        $data = [
+        [
+           "id" => 1,
+           "warehouse_id"=> "2500495P",
+           "icpl"=> "ICPL01",
+           "validfrom"=> "2023-01-03 00:00:00",
+           "active"=> "Y"
+       ],
+       [
+           "id"=> 3,
+           "warehouse_id"=>"2500495P",
+           "icpl"=> "ICPL03",
+           "validfrom"=> "2023-09-01 00:00:00",
+           "active"=> "Y"
+       ]
+       ];
+   
+    }else if($id_m_warehouse_icpl == '2558023P'){
+        $data = [
+        [
+           "id" => 1,
+           "warehouse_id"=> "2558023P",
+           "icpl"=> "ICPL01",
+           "validfrom"=> "2023-01-03 00:00:00",
+           "active"=> "Y"
+       ],
+       [
+           "id"=> 3,
+           "warehouse_id"=>"2558023P",
+           "icpl"=> "ICPL03",
+           "validfrom"=> "2023-09-01 00:00:00",
+           "active"=> "Y"
+       ]
+       ];
+   
+       }else if($id_m_warehouse_icpl == '3500434P'){
+        $data = [
+        [
+           "id" => 1,
+           "warehouse_id"=> "3500434P",
+           "icpl"=> "ICPL01",
+           "validfrom"=> "2023-01-03 00:00:00",
+           "active"=> "Y"
+       ],
+       [
+           "id"=> 3,
+           "warehouse_id"=>"3500434P",
+           "icpl"=> "ICPL03",
+           "validfrom"=> "2023-09-01 00:00:00",
+           "active"=> "Y"
+       ]
+       ];
+   
+       }else if($id_m_warehouse_icpl == '3500525P'){
+        $data = [
+        [
+           "id" => 1,
+           "warehouse_id"=> "3500525P",
+           "icpl"=> "ICPL01",
+           "validfrom"=> "2023-01-03 00:00:00",
+           "active"=> "Y"
+       ],
+       [
+           "id"=> 3,
+           "warehouse_id"=>"3500525P",
+           "icpl"=> "ICPL03",
+           "validfrom"=> "2023-09-01 00:00:00",
+           "active"=> "Y"
+       ]
+       ];
+   
+       }else if($id_m_warehouse_icpl == '3500526P'){
+        $data = [
+        [
+           "id" => 1,
+           "warehouse_id"=> "3500526P",
+           "icpl"=> "ICPL01",
+           "validfrom"=> "2023-01-03 00:00:00",
+           "active"=> "Y"
+       ],
+       [
+           "id"=> 3,
+           "warehouse_id"=>"3500526P",
+           "icpl"=> "ICPL03",
+           "validfrom"=> "2023-09-01 00:00:00",
+           "active"=> "Y"
+       ]
+       ];
+   
+       }else if($id_m_warehouse_icpl == '3500584P'){
+        $data = [
+        [
+           "id" => 1,
+           "warehouse_id"=> "3500584P",
+           "icpl"=> "ICPL01",
+           "validfrom"=> "2023-01-03 00:00:00",
+           "active"=> "Y"
+       ],
+       [
+           "id"=> 3,
+           "warehouse_id"=>"3500584P",
+           "icpl"=> "ICPL03",
+           "validfrom"=> "2023-09-01 00:00:00",
+           "active"=> "Y"
+       ]
+       ];
+   
+       }else if($id_m_warehouse_icpl == '3500414P'){
+        $data = [
+        [
+           "id" => 1,
+           "warehouse_id"=> "3500414P",
+           "icpl"=> "ICPL01",
+           "validfrom"=> "2023-01-03 00:00:00",
+           "active"=> "Y"
+       ],
+       [
+           "id"=> 3,
+           "warehouse_id"=>"3500414P",
+           "icpl"=> "ICPL03",
+           "validfrom"=> "2023-09-01 00:00:00",
+           "active"=> "Y"
+       ]
+       ];
+   
+       }else if ($id_m_warehouse_icpl == '3500414P'){
+      $data = [
+            [
+              "id" => 1,
+              "warehouse_id"=> '3500414P',
+              "icpl"=> "ICPL015",
+              "validfrom"=> "2023-01-03 00:00:00",
+              "active"=> "Y"
+          ],
+          [
+              "id"=> 3,
+              "warehouse_id"=> '3500414P',
+              "icpl"=> "ICPL013",
+              "validfrom"=> "2023-09-01 00:00:00",
+              "active"=> "Y"
+          ]
+          ];
+    }else if ($id_m_warehouse_icpl == '3500495P'){
+        $data = [
+              [
+                "id" => 1,
+                "warehouse_id"=> '3500495P',
+                "icpl"=> "ICPL015",
+                "validfrom"=> "2023-01-03 00:00:00",
+                "active"=> "Y"
+            ],
+            [
+                "id"=> 3,
+                "warehouse_id"=> '3500495P',
+                "icpl"=> "ICPL013",
+                "validfrom"=> "2023-09-01 00:00:00",
+                "active"=> "Y"
+            ]
+            ];
+      }else if ($id_m_warehouse_icpl == '3558023P'){
+        $data = [
+              [
+                "id" => 1,
+                "warehouse_id"=> '3558023P',
+                "icpl"=> "ICPL015",
+                "validfrom"=> "2023-01-03 00:00:00",
+                "active"=> "Y"
+            ],
+            [
+                "id"=> 3,
+                "warehouse_id"=> '3558023P',
+                "icpl"=> "ICPL013",
+                "validfrom"=> "2023-09-01 00:00:00",
+                "active"=> "Y"
+            ]
+            ];
+      }else{
+      return $response = [
+            "status" => 200,
+            "message" => "Bisa Kok Cuman ID nya ga kecatet"
+        ];
+    }
  
 
-    // $response = [
-    //     "status" => 200,
-    //     "message" => "Success",
-    //     "access_token" => "asadaMMsnCAq28NGKqEf6jRueKLsf0wFnAcz2dOrPQF53GYS9AUzwWtiNFKQRniFb",
-    //     "data" => $data
-    // ];
+    $response = [
+        "status" => 200,
+        "message" => "Success",
+        "access_token" => "asadaMMsnCAq28NGKqEf6jRueKLsf0wFnAcz2dOrPQF53GYS9AUzwWtiNFKQRniFb",
+        "data" => $data
+    ];
     header('Content-Type: application/json');
     echo json_encode($response);
 }else{
--- tbl_m_warehouses.php
+++ tbl_m_warehouses.php
@@ -2,53 +2,162 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $company = $_POST['company']; 
     $geturl = $_POST['geturl']; 
+  if($company == 'connSPA'){
     $response = [
         "status" => 200,
-        "message" => "Success",
+        "message" => "Success connSPA",
         "access_token" => "asadaMMsnCAq28NGKqEf6jRueKLsf0wFnAcz2dOrPQF53GYS9AUzwWtiNFKQRniFb",
+        
         "data" => [
             [
-                "name" => "BAZAAR ATRIUM DSM ALL BRAND 1 S/D 30 SEP",
-                "value" => "500434P",
+                "name" => "BAZAAR SPA",
+                "value" => "1500434P",
                 "active" => "Y"
             ],
             [
-                "name" => "BAZAAR BANDARA JUANDA 1 A GATE 8 SURABAY",
-                "value" => "500525P",
+                "name" => "SPA BAZAAR BANDARA JUANDA 1 A GATE 8 SURABAY",
+                "value" => "1500525P",
                 "active" => "Y"
             ],
             [
-                "name" => "BAZAAR BANDARA JUANDA 1 A SURABAYA",
-                "value" => "500526P",
+                "name" => "SPA BAZAAR BANDARA JUANDA 1 A SURABAYA",
+                "value" => "1500526P",
                 "active" => "Y"
             ],
             [
-                "name" => "BAZAAR BANDARA KUALANAMO MEDAN II 2018",
-                "value" => "500584P",
+                "name" => "SPA BAZAAR BANDARA KUALANAMO MEDAN II 2018",
+                "value" => "1500584P",
                 "active" => "Y"
             ],
             [
-                "name" => "BAZAAR BDR BANJARMASIN POLO",
-                "value" => "500414P",
+                "name" => "SPA BAZAAR BDR BANJARMASIN POLO",
+                "value" => "1500414P",
                 "active" => "N"
             ],
             [
-                "name" => "BAZAAR BLR BINTAN",
-                "value" => "500608P",
+                "name" => "SPA BAZAAR BLR BINTAN",
+                "value" => "1500608P",
                 "active" => "Y"
             ],
             [
-                "name" => "BAZAAR BSB BALIKPAPAN (JULI'2016)",
-                "value" => "500495P",
+                "name" => "SPA BAZAAR BSB BALIKPAPAN (JULI'2016)",
+                "value" => "1500495P",
                 "active" => "Y"
             ],
             [
-                "name" => "BAZAAR GAMBIR EXPO KEMAYARON P",
-                "value" => "558023P",
+                "name" => "SPA BAZAAR GAMBIR EXPO KEMAYARON P",
+                "value" => "1558023P",
                 "active" => "N"
             ],
         ]
+   
     ];
+    
+  }else if($company == 'connFnB'){
+    $response = [
+        "status" => 200,
+        "message" => "Success connFnB",
+        "access_token" => "asadaMMsnCAq28NGKqEf6jRueKLsf0wFnAcz2dOrPQF53GYS9AUzwWtiNFKQRniFb",
+        
+        "data" => [
+            [
+                "name" => "FnB BAZAAR ATRIUM DSM ALL BRAND 1 S/D 30 SEP",
+                "value" => "2500434P",
+                "active" => "Y"
+            ],
+            [
+                "name" => "FnB BAZAAR BANDARA JUANDA 1 A GATE 8 SURABAY",
+                "value" => "2500525P",
+                "active" => "Y"
+            ],
+            [
+                "name" => "FnB BAZAAR BANDARA JUANDA 1 A SURABAYA",
+                "value" => "2500526P",
+                "active" => "Y"
+            ],
+            [
+                "name" => "FnB BAZAAR BANDARA KUALANAMO MEDAN II 2018",
+                "value" => "2500584P",
+                "active" => "Y"
+            ],
+            [
+                "name" => "FnB BAZAAR BDR BANJARMASIN POLO",
+                "value" => "2500414P",
+                "active" => "N"
+            ],
+            [
+                "name" => "FnB BAZAAR BLR BINTAN",
+                "value" => "2500608P",
+                "active" => "Y"
+            ],
+            [
+                "name" => "FnB BAZAAR BSB BALIKPAPAN (JULI'2016)",
+                "value" => "2500495P",
+                "active" => "Y"
+            ],
+            [
+                "name" => "FnB BAZAAR GAMBIR EXPO KEMAYARON P",
+                "value" => "2558023P",
+                "active" => "N"
+            ],
+        ]
+   
+    ];
+
+  }else if($company == 'connRetail'){
+    $response = [
+        "status" => 200,
+        "message" => "Success connRetail",
+        "access_token" => "asadaMMsnCAq28NGKqEf6jRueKLsf0wFnAcz2dOrPQF53GYS9AUzwWtiNFKQRniFb",
+        
+        "data" => [
+            [
+                "name" => "Retail BAZAAR ATRIUM DSM ALL BRAND 1 S/D 30 SEP",
+                "value" => "3500434P",
+                "active" => "Y"
+            ],
+            [
+                "name" => "Retail BAZAAR BANDARA JUANDA 1 A GATE 8 SURABAY",
+                "value" => "3500525P",
+                "active" => "Y"
+            ],
+            [
+                "name" => "Retail BAZAAR BANDARA JUANDA 1 A SURABAYA",
+                "value" => "3500526P",
+                "active" => "Y"
+            ],
+            [
+                "name" => "Retail BAZAAR BANDARA KUALANAMO MEDAN II 2018",
+                "value" => "3500584P",
+                "active" => "Y"
+            ],
+            [
+                "name" => "Retail BAZAAR BDR BANJARMASIN POLO",
+                "value" => "3500414P",
+                "active" => "N"
+            ],
+            [
+                "name" => "Retail BAZAAR BLR BINTAN",
+                "value" => "3500608P",
+                "active" => "Y"
+            ],
+            [
+                "name" => "Retail BAZAAR BSB BALIKPAPAN (JULI'2016)",
+                "value" => "3500495P",
+                "active" => "Y"
+            ],
+            [
+                "name" => "Retail BAZAAR GAMBIR EXPO KEMAYARON P",
+                "value" => "3558023P",
+                "active" => "N"
+            ],
+        ]
+   
+    ];
+  }else{
+
+  }
+    
     header('Content-Type: application/json');
     echo json_encode($response);
 }else{
