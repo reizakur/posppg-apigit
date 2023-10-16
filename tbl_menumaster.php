cbfeb0390068606ede9a16dee61c4d38dd56e0c7
reizakur <rheizakurniasmk1@gmail.com>
2023-10-16 11:55:10 +0700

    fdsfds

---
+++ tbl_menumaster.php
@@ -0,0 +1,236 @@
+{
+  "data": [
+            {
+            "id" => "",
+            "name" => "",
+            "active" => "",
+            "created_at" => "",
+            "updated_at" => "",
+            "created_by" => "",
+            "updated_by" => "",
+            "parent_id" => "",
+            "summary" => ""
+            "data_parent" => [{
+                "id" => "",
+                "name" => "",
+                "active" => "",
+                "created_at" => "",
+                "updated_at" => "",
+                "created_by" => "",
+                "updated_by" => "",
+                "parent_id" => "",
+                "summary" => ""
+                "data_produk" => [{
+                        "id" => "",
+                        "value" => "",
+                        "name" => "",
+                        "stocked" => "",
+                        "treatment" => "",
+                        "sold" => "",
+                        "konsinyasi" => "",
+                        "mingguan" => "",
+                        "harian" => "",
+                        "bulanan" => "",
+                        "uom_id" => "",
+                        "category_id" => "",
+                        "active" => "",
+                        "created_at" => "",
+                        "updated_at" => "",
+                        "created_by" => "",
+                        "updated_by" => ""
+                        },
+                        {
+                        "id" => "",
+                        "value" => "",
+                        "name" => "",
+                        "stocked" => "",
+                        "treatment" => "",
+                        "sold" => "",
+                        "konsinyasi" => "",
+                        "mingguan" => "",
+                        "harian" => "",
+                        "bulanan" => "",
+                        "uom_id" => "",
+                        "category_id" => "",
+                        "active" => "",
+                        "created_at" => "",
+                        "updated_at" => "",
+                        "created_by" => "",
+                        "updated_by" => ""
+            },
+            {
+            "id" => "",
+            "value" => "",
+            "name" => "",
+            "stocked" => "",
+            "treatment" => "",
+            "sold" => "",
+            "konsinyasi" => "",
+            "mingguan" => "",
+            "harian" => "",
+            "bulanan" => "",
+            "uom_id" => "",
+            "category_id" => "",
+            "active" => "",
+            "created_at" => "",
+            "updated_at" => "",
+            "created_by" => "",
+            "updated_by" => ""
+            },
+]
+  }
+
+"id" => "",
+"name" => "",
+"active" => "",
+"created_at" => "",
+"updated_at" => "",
+"created_by" => "",
+"updated_by" => "",
+"parent_id" => "",
+"summary" => ""
+
+"id" => "",
+"name" => "",
+"active" => "",
+"created_at" => "",
+"updated_at" => "",
+"created_by" => "",
+"updated_by" => "",
+"parent_id" => "",
+"summary" => ""
+      "data_m_vpospayment": [
+        {
+          "id": "101",
+          "pos_id": "1",
+          "invoicedamt": "1000.00"
+        }
+      ],
+      "data_mposline": [
+        {
+          "id": "201",
+          "pos_id": "1",
+          "product_id": "001",
+          "m_vpos_komisi": [
+            {
+              "id": "301",
+              "pos_id": "1",
+              "posline_id": "201"
+            },
+            {
+              "id": "302",
+              "pos_id": "1",
+              "posline_id": "201"
+            }
+          ],
+          "m_vpos_treatment": [
+            {
+              "id": "401",
+              "pos_id": "1",
+              "posline_id": "201"
+            }
+          ],
+          "m_vposline_promo": [
+            {
+              "id": "501",
+              "pos_id": "1",
+              "posline_id": "201"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": "2",
+      "documentno": "DOC002",
+      "ranno": "RAN002",
+      "data_m_vpospayment": [
+        {
+          "id": "102",
+          "pos_id": "2",
+          "invoicedamt": "1500.00"
+        }
+      ],
+      "data_mposline": [
+        {
+          "id": "202",
+          "pos_id": "2",
+          "product_id": "002",
+          "m_vpos_komisi": [
+            {
+              "id": "302",
+              "pos_id": "2",
+              "posline_id": "202"
+            }
+          ],
+          "m_vpos_treatment": [
+            {
+              "id": "402",
+              "pos_id": "2",
+              "posline_id": "202"
+            }
+          ],
+          "m_vposline_promo": [
+            {
+              "id": "502",
+              "pos_id": "2",
+              "posline_id": "202"
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  "datanonquery": [
+    {
+      "id": "301",
+      "pos_id": "1",
+      "posline_id": "201"
+    },
+    {
+      "id": "302",
+      "pos_id": "1",
+      "posline_id": "201"
+    },
+    {
+      "id": "401",
+      "pos_id": "1",
+      "posline_id": "201"
+    },
+    {
+      "id": "501",
+      "pos_id": "1",
+      "posline_id": "201"
+    },
+    {
+      "id": "2",
+      "documentno": "DOC002",
+      "ranno": "RAN002"
+    },
+    {
+      "id": "102",
+      "pos_id": "2",
+      "invoicedamt": "1500.00"
+    },
+    {
+      "id": "202",
+      "pos_id": "2",
+      "product_id": "002"
+    },
+    {
+      "id": "302",
+      "pos_id": "2",
+      "posline_id": "202"
+    },
+    {
+      "id": "402",
+      "pos_id": "2",
+      "posline_id": "202"
+    },
+    {
+      "id": "502",
+      "pos_id": "2",
+      "posline_id": "202"
+    }
+  ]
+}
