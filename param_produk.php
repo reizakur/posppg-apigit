27d356fc65683e8c2c6347bfbd0118885a683ddb
reizakur <rheizakurniasmk1@gmail.com>
2023-10-16 11:49:55 +0700

    asdad

--- param_produk.php
+++ param_produk.php
@@ -2,98 +2,280 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $company = $_POST['company']; 
     $geturl = $_POST['geturl']; 
-    $id_product = $_POST['id_product'];
-    if($id_product == '002'){
-     $data = [
-        [
-            "id" => 2,
-            "value" => "002",
-            "name" => "Bolu terbang",
-            "stocked" => "N",
-            "treatment" => "N",
-            "sold" => "N",
-            "konsinyasi" => "N",
-            "mingguan" => "N",
-            "harian" => "N",
-            "bulanan" => "N",
-            "uom_id" => 1,
-            "category_id" => 1,
-            "active" => "Y",
-            "created_at" => "2023-09-15 09:40:40.641",
-            "updated_at" => "2023-09-15 09:40:40.641",
-            "created_by" => 1,
-            "updated_by" => 1
- ],
- [
-    "id" => 3,
-    "value" => "002",
-    "name" => "Makan Enak",
-    "stocked" => "N",
-    "treatment" => "N",
-    "sold" => "N",
-    "konsinyasi" => "N",
-    "mingguan" => "N",
-    "harian" => "N",
-    "bulanan" => "N",
-    "uom_id" => 1,
-    "category_id" => 1,
-    "active" => "Y",
-    "created_at" => "2023-09-15 09:40:40.641",
-    "updated_at" => "2023-09-15 09:40:40.641",
-    "created_by" => 1,
-    "updated_by" => 1
-],
-    ];
-
-    }else if ($id_product == '003'){
-       $data = [
-        [
-            "id" => 2,
-            "value" => "003",
-            "name" => "Bolu terbang",
-            "stocked" => "N",
-            "treatment" => "N",
-            "sold" => "N",
-            "konsinyasi" => "N",
-            "mingguan" => "N",
-            "harian" => "N",
-            "bulanan" => "N",
-            "uom_id" => 1,
-            "category_id" => 1,
-            "active" => "Y",
-            "created_at" => "2023-09-15 09:40:40.641",
-            "updated_at" => "2023-09-15 09:40:40.641",
-            "created_by" => 1,
-            "updated_by" => 1
- ],
- [
-    "id" => 3,
-    "value" => "003",
-    "name" => "Makan Enak",
-    "stocked" => "N",
-    "treatment" => "N",
-    "sold" => "N",
-    "konsinyasi" => "N",
-    "mingguan" => "N",
-    "harian" => "N",
-    "bulanan" => "N",
-    "uom_id" => 1,
-    "category_id" => 1,
-    "active" => "Y",
-    "created_at" => "2023-09-15 09:40:40.641",
-    "updated_at" => "2023-09-15 09:40:40.641",
-    "created_by" => 1,
-    "updated_by" => 1
-],
-           ];
-    }else{
-      return $response = [
-            "status" => 200,
-            "message" => "Bisa Kok Cuman ID nya ga kecatet"
-        ];
+    $icpl = $_POST['icpl'];
+    echo("tes777");
+    echo($icpl);
+    if ($icpl == "ICPL29") {
+        echo("tes333"); 
+        $icplValue = "ICPL29";
+        $effectiveDate = "2023-10-01";
+        $priceValue = "100.00";
+    } elseif ($icpl == "ICPL30") {
+        echo("tes111");
+        $icplValue = "ICPL30";
+        $effectiveDate = "2023-10-02";
+        $priceValue = "150.00";
+    } elseif ($icpl == "ICPL31") {
+        echo("tes222");
+        $icplValue = "ICPL31";
+        $effectiveDate = "2023-10-03";
+        $priceValue = "200.00";
+    } else {
+        echo("tes444");
+        $icplValue = "DefaultICPL";
+        $effectiveDate = "2023-10-01";
+        $priceValue = "100.00";
     }
- 
+    
+  
+  $data = [ 
+                "data_m_menu_category_y" => [
+                    [
+                        "id_y" => "1",
+                        "name" => "Category Y1",
+                        "active" => "true",
+                        "created_at" => "2023-10-01",
+                        "updated_at" => "2023-10-01",
+                        "created_by" => "User1",
+                        "updated_by" => "User2",
+                        "parent_id" => "",
+                        "summary" => "Summary for Category Y1",
+                        "data_m_menu_category_n" => [
+                            [
+                                "id_n" => "1",
+                                "name" => "Category N1",
+                                "active" => "true",
+                                "created_at" => "2023-10-01",
+                                "updated_at" => "2023-10-01",
+                                "created_by" => "User1",
+                                "updated_by" => "User2",
+                                "parent_id" => "1",
+                                "summary" => "Summary for Category N1",
+                                "data_m_product_menu"=> []
+                            ]
+                        ],
+                        [
+                            "id_y" => "2",
+                            "name" => "Category Y2",
+                            "active" => "true",
+                            "created_at" => "2023-10-01",
+                            "updated_at" => "2023-10-01",
+                            "created_by" => "User1",
+                            "updated_by" => " User2",
+                            "parent_id" => "",
+                            "summary" => "Summary for Category Y2",
+                            "data_m_menu_category_n" => []
+                        ],
+                        [
+                            "id_y" => "3",
+                            "name" => "Category Y3",
+                            "active" => "true",
+                            "created_at" => "2023-10-01",
+                            "updated_at" => "2023-10-01",
+                            "created_by" => "User1",
+                            "updated_by" => "User2",
+                            "parent_id" => "",
+                            "summary" => "Summary for Category Y3",
+                            "data_m_menu_category_n" => [
+                                [
+                                    "id_n" => "1",
+                                    "name" => "Category N1",
+                                    "active" => "true",
+                                    "created_at" => "2023-10-01",
+                                    "updated_at" => "2023-10-01",
+                                    "created_by" => "User1",
+                                    "updated_by" => "User2",
+                                    "parent_id" => "3",
+                                    "summary" => "Summary for Category N1",
+                                    "data_m_product_menu" => [
+                                        [
+                                            "id_m_product_menu" => "1",
+                                            "product_id" => "Product1",
+                                            "menu_category_id" => "1",
+                                            "active_m_product_menu" => "true",
+                                            "id_m_product" => "1",
+                                            "value" => "100.00",
+                                            "name" => "Product Name 1",
+                                            "stocked" => "true",
+                                            "treatment" => "Treatment1",
+                                            "sold" => "Sold1",
+                                            "konsinyasi" => "Konsinyasi1",
+                                            "mingguan" => "Mingguan1",
+                                            "harian" => "Harian1",
+                                            "bulanan" => "Bulanan1",
+                                            "uom_id" => "1",
+                                            "category_id" => "1",
+                                            "active_m_product" => "Y",
+                                            "created_at" => "2023-10-01",
+                                            "updated_at" => "2023-10-01",
+                                            "created_by" => "User1",
+                                            "updated_by" => "User2",
+                                            "id_m_product_price" => "1",
+                                            "icpl" => "$icplValue",
+                                            "effective" => "$effectiveDate",
+                                            "price" => "$priceValue"
+                                        ],
+                                        [
+                                            "id_m_product_menu" => "2",
+                                            "product_id" => "Product2",
+                                            "menu_category_id" => "1",
+                                            "active_m_product_menu" => "true",
+                                            "id_m_product" => "2",
+                                            "value" => "200.00",
+                                            "name" => "Product Name 2",
+                                            "stocked" => "true",
+                                            "treatment" => "Treatment2",
+                                            "sold" => "Sold2",
+                                            "konsinyasi" => "Konsinyasi2",
+                                            "mingguan" => "Mingguan2",
+                                            "harian" => "Harian2",
+                                            "bulanan" => "Bulanan2",
+                                            "uom_id" => "2",
+                                            "category_id" => "2",
+                                            "active_m_product" => "Y",
+                                            "created_at" => "2023-10-01",
+                                            "updated_at" => "2023-10-01",
+                                            "created_by" => "User1",
+                                            "updated_by" => "User2",
+                                            "id_m_product_price" => "2",
+                                            "icpl" => "$icplValue",
+                                            "effective" => "$effectiveDate",
+                                            "price" => "$priceValue"
+                                        ]
+                                    ]
+                                ],
+                                [
+                                    "id_n" => "2",
+                                    "name" => "Category N2",
+                                    "active" => "true",
+                                    "created_at" => "2023-10-01",
+                                    "updated_at" => "2023-10-01",
+                                    "created_by" => "User1",
+                                    "updated_by" => "User2",
+                                    "parent_id" => "3",
+                                    "summary" => "Summary for Category N2",
+                                    "data_m_product_menu" => [
+                                        [
+                                            "id_m_product_menu" => "3",
+                                            "product_id" => "Product3",
+                                            "menu_category_id" => "2",
+                                            "active_m_product_menu" => "true",
+                                            "id_m_product" => "3",
+                                            "value" => "300.00",
+                                            "name" => "Product Name 3",
+                                            "stocked" => "true",
+                                            "treatment" => "Treatment3",
+                                            "sold" => "Sold3",
+                                            "konsinyasi" => "Konsinyasi3",
+                                            "mingguan" => "Mingguan3",
+                                            "harian" => "Harian3",
+                                            "bulanan" => "",
+                                            "uom_id" => "",
+                                            "category_id" => "",
+                                            "active_m_product" => "Y",
+                                            "created_at" => "2023-10-",
+                                            "updated_at" => "2023-10-",
+                                            "created_by" => "2023-10-01",
+                                            "updated_by" => "2023-10-01",
+                                            "id_m_product_price" => "3",
+                                            "icpl" => "$icplValue",
+                                            "effective" => "$effectiveDate",
+                                            "price" => "$priceValue"
+                                        ],
+                                    ]
+                                ],
+                            ],
+                        ]
+                    ]
+                ], 
+                [
+                    "data_m_product" => [
+                        [
+                            "id_m_product_menu" => "1",
+                            "product_id" => "Product1",
+                            "menu_category_id" => "1",
+                            "active_m_product_menu" => "true",
+                            "id_m_product" => "1",
+                            "value" => "100.00",
+                            "name" => "Product Name 1",
+                            "stocked" => "true",
+                            "treatment" => "Treatment1",
+                            "sold" => "Sold1",
+                            "konsinyasi" => "Konsinyasi1",
+                            "mingguan" => "Mingguan1",
+                            "harian" => "Harian1",
+                            "bulanan" => "Bulanan1",
+                            "uom_id" => "1",
+                            "category_id" => "1",
+                            "active_m_product" => "true",
+                            "created_at" => "2023-10-01",
+                            "updated_at" => "2023-10-01",
+                            "created_by" => "User1",
+                            "updated_by" => "User2",
+                            "id_m_product_price" => "1",
+                            "icpl" => "$icplValue",
+                            "effective" => "$effectiveDate",
+                            "price" => "$priceValue"
+                        ],
+                        [
+                            "id_m_product_menu" => "2",
+                            "product_id" => "Product2",
+                            "menu_category_id" => "1",
+                            "active_m_product_menu" => "true",
+                            "id_m_product" => "2",
+                            "value" => "200.00",
+                            "name" => "Product Name 2",
+                            "stocked" => "true",
+                            "treatment" => "Treatment2",
+                            "sold" => "Sold2",
+                            "konsinyasi" => "Konsinyasi2",
+                            "mingguan" => "Mingguan2",
+                            "harian" => "Harian2",
+                            "bulanan" => "Bulanan2",
+                            "uom_id" => "2",
+                            "category_id" => "2",
+                            "active_m_product" => "true",
+                            "created_at" => "2023-10-01",
+                            "updated_at" => "2023-10-01",
+                            "created_by" => "User1",
+                            "updated_by" => "User2",
+                            "id_m_product_price" => "2",
+                            "icpl" => "$icplValue",
+                            "effective" => "$effectiveDate",
+                            "price" => "$priceValue"
+                        ],
+                        [
+                            "id_m_product_menu" => "3",
+                            "product_id" => "Product3",
+                            "menu_category_id" => "2",
+                            "active_m_product_menu" => "true",
+                            "id_m_product" => "3",
+                            "value" => "300.00",
+                            "name" => "Product Name 3",
+                            "stocked" => "true",
+                            "treatment" => "Treatment3",
+                            "sold" => "Sold3",
+                            "konsinyasi" => "Konsinyasi3",
+                            "mingguan" => "Mingguan3",
+                            "harian" => "Harian3",
+                            "bulanan" => "",
+                            "uom_id" => "",
+                            "category_id" => "",
+                            "active_m_product" => "",
+                            "created_at" => "2023-10-",
+                            "updated_at" => "2023-10-",
+                            "created_by" => "",
+                            "updated_by" => "",
+                            "id_m_product_price" => "3",
+                            "icpl" => "$icplValue",
+                            "effective" => "$effectiveDate",
+                            "price" => "$priceValue"
+                        ],
+                    ]
+                ]
+            ];
 
+echo("tes123");
     $response = [
         "status" => 200,
         "message" => "Success",
